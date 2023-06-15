ea85b518fb2b09d111238af628bb8b565e061794
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2023-06-15 15:49:19 +0700

    string element step 3

--- stackArray_115/stackArray_115.cpp
+++ stackArray_115/stackArray_115.cpp
@@ -17,3 +17,11 @@ public:
 			cout << "Number of data exceeds the limit" << endl;
 			return "";
 		}
+
+		top++; //step 2
+		stack_array[top] = element; //step3
+		cout << endl;
+		cout << element << "ditambah(pushed)" << endl;
+
+		return element;
+	}
