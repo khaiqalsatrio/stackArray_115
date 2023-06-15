3cf9267db78fb522eed1b0029bd71c7952c76995
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2023-06-15 15:40:38 +0700

    Add project files.

---
+++ stackArray_115/stackArray_115.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class stackArray {
+private:
+	string stack_array[5];
+	int top;
+public:
+	//construktor
+	stackArray() {
+		top = -1;
+	}
+
+	string push(string element) {
+		if (top == 4) {	//step 1
+			cout << "Number of data exceeds the limit" << endl;
+			return "";
+		}
+
+		top++; //step 2
+		stack_array[top] = element; //step3
+		cout << endl;
+		cout << element << "ditambah(pushed)" << endl;
+
+		return element;
+	}
+
+	void pop() {
+		if (empty()) { //step4
+			cout << "\nstack is empty. cannot pop." << endl;	//1a
+			return; //1.b
+		}
+
+		cout << "\nthe poped element is : " << stack_array[top] << endl; //step2
+		top--; //step 3 decrement
+	}
+
+	//method for check if data is empty
+	bool empty() {
+		return (top == -1);
+	}
+
+	void display() {
+		if (empty())
+			cout << "\nstack is empty." << endl;
+
+		else {
+			for (int tmp = 0; tmp <= top; tmp++) {
+				cout << stack_array[tmp] << endl;
+			}
+		}
+	}
+};
+
+int main() {
+	stackArray s;
+	while (true) {
+		cout << endl;
+		cout << "\n**stack menu**\n";
+		cout << "1. push\n";
+		cout << "2. pop\n";
+		cout << "3. display\n";
+		cout << "4. exit\n";
+		cout << "\nEnter your choice: ";
+		string input;
+		getline(cin, input);
+		char ch = (input.empty() ? '0' : input[0]);
+		switch (ch) {
+		case '1': {
+			cout << "\nEnter a element: ";
+			string element;
+			getline(cin, element);
+			s.push(element);
+			break;
+		}
+		case '2':
+			if (s.empty()) {
+				cout << "\nstack in empty." << endl;
+				break;
+			}
+			s.pop();
+			break;
+		case '3':
+			s.display();
+			break;
+		case '4':
+			return 0;
+		default:
+			cout << "\nInvalid choice." << endl;
+			break;
+		}
+	}
+	return 0;
+}
